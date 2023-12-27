74a99dcd325b0d338f33d13f31f6215e450a9a3e
Ayush Jha <ayushjha090@gmail.com>
2023-12-27 23:34:30 +0530

    made dialog box responsive

--- src/components/UI/Dialog/index.tsx
+++ src/components/UI/Dialog/index.tsx
@@ -8,7 +8,7 @@ const Dialog: FC<Props> = ({ children, open }) => {
     <>
       {open ? 
         <div 
-          className="w-5/12 absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 p-5 rounded-md bg-primary-color-light z-50 shadow-md shadow-primary-color-dark"
+          className="w-10/12 md:w-8/12 lg:w-2/5 absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 p-5 rounded-md bg-primary-color-light z-50 shadow-md shadow-primary-color-dark"
         >
           {children}
         </div> : null
